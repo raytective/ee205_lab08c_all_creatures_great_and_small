44e324fb6459d455eaa59c7187821b8a0e624306
raytective <rkwatana@hawaii.edu>
2022-03-08 19:12:29 -1000

    finished stubbing out all code

--- main.cpp
+++ main.cpp
@@ -9,8 +9,12 @@
 /// @date   08_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
+#include "Animal.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Animal myFirstAnimal = Animal( "Felis catus" );
+    myFirstAnimal.setGender( MALE );
+    myFirstAnimal.setWeight( 19 ); // @todo Refactor to include Unit of Measure
+    myFirstAnimal.printInfo();
     return 0;
 }
